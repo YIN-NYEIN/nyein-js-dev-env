7f4a3141a9db2bce612c71cc600384acab4419bd
administrator <administrator@dll-mm013-MacBook-Air.local>
2018-07-13 09:22:20 +0630

    New Appveyor

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,7 +6,7 @@ const path = require ('path');
 const open = require ('open');
 const webpack = require ('webpack');
 //const config = require ('config');
-const config = require('webpack.config.dev.js');
+const config = require('../src/webpack.config.dev.js');
 const port = 3000;
 
 
@@ -22,6 +22,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.get('/', function(req, res){
 res.sendFile(path.join(__dirname, '../src/index.html'));
 });//tell Express which routes it should handle
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
 
 app.listen(port,function(err){
     if(err){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './index.css';
-const numeral = require ('numeral');
 
-const courseValue = numeral (1000).format('$0,0.00');
-debugger;
-console.log('I would play ' , courseValue  , 'for this awesome course!');
+//import {getUsers} from './api/userApi';
+var getUsers = require('./src/api/userApi');
+
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody
+});
